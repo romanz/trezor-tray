e61cacebe225ee299528a09c4b27b4d6e63d8150
Roman Zeyde <me@romanzey.de>
2018-03-05 18:14:15 +0200

    handle USB events

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import sys
 from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtCore import QTimer
 
+from pyudev import Context, Monitor
+from pyudev.pyqt5 import MonitorObserver
+
 from libagent.device import trezor
 from libagent import util
 
@@ -12,6 +15,8 @@ log = logging.getLogger(__name__)
 
 
 class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
+
+    INTERVAL = 60 * 1000  # 60 seconds
 
     def __init__(self, icon, parent, app):
         super().__init__(icon, parent)
@@ -26,16 +31,28 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
         self.device = trezor.Trezor()
         self.ping()
 
+        self._observe()
+
         self.timer = QTimer()
         self.timer.timeout.connect(self.on_timer)
-        self.timer.start(60e3)  # ping every minute (in ms)
+        self.timer.start(self.INTERVAL)
+
+    def _observe(self):
+        self.context = Context()
+        self.monitor = Monitor.from_netlink(self.context)
+        self.monitor.filter_by(subsystem='hid')
+        self.observer = MonitorObserver(self.monitor)
+        self.observer.deviceEvent.connect(self.on_device_event)
+        self.monitor.start()
 
     def ping(self):
         try:
             with self.device:
                 log.info('unlocked')
+                self.setToolTip('Unlocked')
         except Exception as e:
-            self.showMessage('Failed to unlock', str(e))
+            log.exception('ping failed: %s', e)
+            self.setToolTip('Failed to unlock: {}'.format(e))
 
     def on_timer(self):
         self.ping()
@@ -47,6 +64,13 @@ class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
     def on_click(self, reason):
         if reason == QtWidgets.QSystemTrayIcon.Trigger:
             self.ping()
+
+    def on_device_event(self, d):
+        log.info('%s %s', d.action, d)
+        if (d.get('HID_NAME') == 'SatoshiLabs TREZOR' and
+            d.get('HID_PHYS', '').endswith('/input0')):
+            if d.action == 'add':
+                self.ping()
 
 
 def main():
